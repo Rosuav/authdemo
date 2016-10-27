f8fa2caaed5664165ac4d7fffa171e1722ee03fa
Chris Angelico <rosuav@gmail.com>
2016-10-27 21:54:52 +1100

    server: Switch to hashed passwords

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 var express = require('express');
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
+var bcrypt = require('bcryptjs');
 
 const users = [
-	{"username": "rosuav", "password": "correct horse battery staple"},
-	{"username": "", "password": ""}
+	{"username": "rosuav", "password": "$2a$10$GbKgyYMQ6UBRZ32yBcq3TulWKid6ACzQbmOBqBiG.48QBtpC89z9O"},
 ];
+//Syntactically-valid password, used to prevent timing-based attacks.
+const dummy = "$2a$10$09NlRCNJ4IPGwpwwdtW8iechXLyO3cs3XV1sl/RMtmf/rOSL1.fIG";
 
 passport.use(new BasicStrategy((username, pwd, cb) => {
-	for (var user of users) {
-		if (user.username === username || user.username === "") {
-			if (user.password === pwd && user.username !== "")
-				return cb(null, user);
-			//Note: If the password is wrong, we continue searching.
-			//This reduces the likelihood of timing-based attacks,
-			//which could otherwise hint that a user exists.
-			//We will ALWAYS perform one password verification.
-			//TODO: Guarantee *exactly* one verification.
-		}
-	}
-	return cb(null, false, "Incorrect user or password");
+	//To prevent timing-based attacks, we always perform exactly ONE
+	//bcrypt comparison, whether the user name was found or not. (The
+	//time cost of searching the array is now negligible compared to
+	//the cost of bcrypting.) The time from entering this function to
+	//calling the callback will be fairly consistent, whether the user
+	//or password is the error.
+	var user = users.find(user => user.username == username);
+	bcrypt.compare(pwd, user ? user.password : dummy, (err, match) => {
+		if (err)
+			cb(err);
+		else if (match && user)
+			cb(null, user);
+		else
+			cb(null, false, "Incorrect user or password");
+	});
 }));
 
 var app = express();
