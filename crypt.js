0e175aa910a3b37f5f40cbd22fcb3f24716fb832
Chris Angelico <rosuav@gmail.com>
2016-10-28 00:43:53 +1100

    crypt: Implement XKCD 936 generation, sorta-kinda

--- crypt.js
+++ crypt.js
@@ -4,9 +4,17 @@ if (process.argv.length > 2) {
 	//Password provided on command line
 	pwd = process.argv.slice(2).join(" ");
 } else {
-	console.error("TODO: Generate pwd from /usr/share/dict/words");
-	pwd = "correct horse battery staple";
-	console.log("Generating password:", pwd);
+	console.error("No password specified - generating from /usr/share/dict/words");
+	var fs = require('fs');
+	var words = fs.readFileSync('/usr/share/dict/words', 'utf8').split('\n');
+	pwd = "";
+	for (var i = 0; i < 4; ++i) {
+		var word = words[Math.floor(Math.random() * words.length)];
+		if (!/^[a-z]+$/.exec(word)) --i; //Ignore words with capitals or apostrophes
+		else pwd += " " + word;
+	}
+	pwd = pwd.trim();
+	console.log("Generated password:", pwd);
 }
 
 var bcrypt = require('bcryptjs');
